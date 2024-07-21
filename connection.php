d53896dc1921c2e0d22d7f99b7bbd15e768871a5
kunal7216 <kk2239901@gmail.com>
2024-07-21 11:23:34 +0530

    first commit

---
+++ connection.php
@@ -0,0 +1,10 @@
+
+        <?php
+        define('hostname', 'localhost');
+        define('user', 'username');
+        define('password', 'pass');
+        define('databaseName', 'testDatabase');
+        
+        $connect = new mysqli(hostname, user, password, databaseName);
+        
+        ?>
